7fc756b5783a3042a47f46ba0a4a70188f886c65
Raj Kumar Balaganesan <errajkumar.aut@gmail.com>
2026-04-09 23:02:58 +0530

    message

---
+++ javascript-training/debug.js
@@ -0,0 +1,13 @@
+console.log("Print 1")
+console.log("Print 2")
+console.log("Print 3")
+SumOfNumbers(10,5);
+console.log("Print 4")
+console.log("Print 5")
+console.log("c")
+
+function SumOfNumbers(a,b)
+{
+c=a+b
+console.log(c);
+}
---
+++ javascript-training/language-basics/hello-world.js
@@ -0,0 +1,4 @@
+console.log("Hello World, Raj Kumar Balaganesan");
+console.error("This is Error Message");
+console.warn("This is Console Warning");
+console.info("This is the informational Message");
